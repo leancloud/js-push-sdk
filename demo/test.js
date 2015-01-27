e794ce175ef5dc3346cd5ffb93755fe59fdee419
尼奥 <1988wangxiao@gmail.com>
2015-01-27 14:31:12 +0800

    [feature] 简易版本 demo 完善。

--- demo/test.js
+++ demo/test.js
@@ -12,21 +12,32 @@ function createNew() {
         appKey: appKey
     });
 
+    // 可以链式调用
     push.open(function() {
         console.log('可以接收推送');
     });
 
+    // 监听推送消息
     push.on('message', function(data) {
         console.log('message');
         console.log(data);
     });
 
+    // 监听网络异常
     push.on('reuse', function() {
-        console.log('reuse');
+        console.log('网络中断正在重试');
     });
 
+    // 发送一条推送
     push.send({
         channels: ['aaa'],
         data: {wangxiao: 123}
     });
+
+    setTimeout(function() {
+        // 如果不加 channels，可以简单的使用 send 方法发送一个 json
+        push.send({
+            abc: 123
+        });
+    }, 5000);
 }
