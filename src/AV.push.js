3695a9bc963825d026be3a0db87bec3c50877457
尼奥 <1988wangxiao@gmail.com>
2015-03-30 11:00:28 +0800

    [bugfix] Event center forgot clean event list.

--- src/AV.push.js
+++ src/AV.push.js
@@ -592,7 +592,7 @@ void function(win) {
                             eventList[eventName][i].call(this, data);
                         }
                     }
-                    cleanNull(eventList[eventName]);
+                    eventList[eventName] = cleanNull(eventList[eventName]);
                 }
                 if (eventOnceList[eventName]) {
                     i = 0;
@@ -603,7 +603,7 @@ void function(win) {
                             _off(eventName, eventOnceList[eventName][i], true);
                         }
                     }
-                    cleanNull(eventOnceList[eventName]);
+                    eventOnceList[eventName] = cleanNull(eventOnceList[eventName]);
                 }
                 return this;
             },
